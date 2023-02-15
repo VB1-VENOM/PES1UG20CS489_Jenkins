2579dd045c0fa10b1332a2e030fe68930dd24572
Varun Satheesh <varunsatheesh55dev@gmail.com>
2023-02-15 11:10:26 +0530

    commiting files to git

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "I am PES1UG10CS489\n";
+  cout << "I have successfully built and run\n";
+  cout << "a C++ program using the g++ compiler\n";
+  return 0;
+}
